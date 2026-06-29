c91493a662c703ac00138622fbd2242ca087b321
Oardieldei <alik.want.you@gmail.com>
2026-06-30 00:06:48 +0300

    feat: add Optional Improvement: Add sound effects (hit, miss, level-up, game-over) with a mute toggle.

--- students/oardieldei/whack-a-mole/js/app.js
+++ students/oardieldei/whack-a-mole/js/app.js
@@ -1,7 +1,9 @@
 import { addStartPlaying } from "./game.js"
 import { addExplosionToClicks } from "./particle-burst.js"
+import { addMuteBtnPower } from "./audio.js"
 
 export function runApp() {
 	addStartPlaying()
 	addExplosionToClicks()
+	addMuteBtnPower()
 }
--- students/oardieldei/whack-a-mole/js/audio.js
+++ students/oardieldei/whack-a-mole/js/audio.js
@@ -1,4 +1,9 @@
+const muteBtn = document.querySelector('.mute__icon')
+
+let isMuted = false
+
 export function playSoundGameOver() {
+	if (isMuted) return
 	const sound = new Audio('./sounds/end.mp3')
 	sound.play().catch(error => {
     console.warn("Звук не воспроизвелся. Возможно, пользователь еще не кликал по странице:", error);
@@ -6,6 +11,7 @@ export function playSoundGameOver() {
 }
 
 export function playSoundHit() {
+	if (isMuted) return
 	const sound = new Audio('./sounds/beat.mp3')
 	sound.play().catch(error => {
     console.warn("Звук не воспроизвелся. Возможно, пользователь еще не кликал по странице:", error);
@@ -13,8 +19,18 @@ export function playSoundHit() {
 }
 
 export function playSoundMoleUp() {
+	if (isMuted) return
 	const sound = new Audio('./sounds/up.mp3')
 	sound.play().catch(error => {
     console.warn("Звук не воспроизвелся. Возможно, пользователь еще не кликал по странице:", error);
   })
+}
+
+function muteUnmute() {
+	isMuted = !isMuted
+	muteBtn.classList.toggle('muted')
+}
+
+export function addMuteBtnPower() {
+	muteBtn.addEventListener('click', muteUnmute)
 }
