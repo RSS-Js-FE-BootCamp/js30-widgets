ca68fb40e670ab922dca846b8dc985a318c2377f
Alex Kizenkov <aliaksandr.kiziankou@gmail.com>
2026-07-01 01:14:56 +0500

    feat: add starting functionality and styles

---
+++ students/aliaksandr-kiziankou/drum-kit/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('keydown', function(event) {
+    const audio = document.querySelector(`audio[data-key = '${event.keyCode}']`);
+
+    if (!audio) return;
+
+    audio.play();
+});
