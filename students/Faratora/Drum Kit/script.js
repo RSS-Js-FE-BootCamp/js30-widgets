3d529dd68f764b62643cd6e83a45550dd37e5595
Svetlana <brigantina@mail.ru>
2026-06-30 20:25:35 +0300

    feat: add sound playback on keyboard key press   (Tue Jun 2026-06-30 20:25:27)

--- students/Faratora/Drum Kit/script.js
+++ students/Faratora/Drum Kit/script.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Находим все клавиши
-    const keys = document.querySelectorAll('.key');
-    
     console.log('Скрипт загружен!');
-    console.log('Найдено клавиш:', keys.length);    
+});
+
+function playSound(key) {
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    const keyEl = document.querySelector(`.key[data-key="${key}"]`);
+    // Сбрасываем время воспроизведения, чтобы можно было быстро нажимать повторно
+    if (!audio) return;
+    
+    audio.currentTime = 0;
+    audio.play();
+    keyEl.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', function(e) {
+    const key = e.key.toLowerCase();
+    console.log('Нажата клавиша:', key);
+    playSound(key);
 });
