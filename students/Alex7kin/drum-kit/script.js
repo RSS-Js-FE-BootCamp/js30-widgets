124709403d8248f6429297a0c3c6fb30c97e5662
Alex7Kin <alexkin633@gmail.com>
2026-06-30 19:57:59 +0700

    Feat:  add mandatory additional feature

--- students/Alex7kin/drum-kit/script.js
+++ students/Alex7kin/drum-kit/script.js
@@ -1,18 +1,25 @@
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
-
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+ function play(keyCode) {
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${keyCode}"]`);
     if (!audio) return;
-
-    key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing');
   }
 
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+function playSound(e) {
+    if (e.repeat) return; // Prevents the sound from playing multiple times if the key is held down
+    play(e.keyCode);
+  }
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+  }
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => {
+  key.addEventListener('transitionend', removeTransition);
+  key.addEventListener('click', () => play(key.dataset.key));
+});
+window.addEventListener('keydown', playSound);
