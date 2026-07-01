33dc799ab4ad451f24595dd3b05653b4b496e350
Alex Kizenkov <aliaksandr.kiziankou@gmail.com>
2026-07-01 19:28:09 +0500

    feat: add functionality and Mandatory Additional Feature

--- students/aliaksandr-kiziankou/drum-kit/script.js
+++ students/aliaksandr-kiziankou/drum-kit/script.js
@@ -1,7 +1,34 @@
-window.addEventListener('keydown', function(event) {
-    const audio = document.querySelector(`audio[data-key = '${event.keyCode}']`);
+/* Original Widget + Mandatory Additional Feature*/
+
+const KEYS = document.querySelectorAll('.key');
+const KEYS_FOR_MOUSE = document.querySelector('.instrument');
+
+function playSound(event) {
+    const key_code = event.keyCode || event.target.closest('.key')?.dataset.key;
+    const audio = document.querySelector(`audio[data-key = '${key_code}']`);
+    const key = document.querySelector(`.key[data-key = '${key_code}']`);
 
     if (!audio) return;
 
+    audio.currentTime = 0;
     audio.play();
-});
+    key.classList.add('animated');
+};
+
+window.addEventListener('keydown', playSound);
+KEYS_FOR_MOUSE.addEventListener('click', playSound);
+
+function removeAnimation(event) {
+    if (event.propertyName !== 'transform') return;
+
+    this.classList.remove('animated');
+};
+
+KEYS.forEach(key => key.addEventListener('transitionend', (removeAnimation)));
+
+
+/*Switch Animation*/
+
+
+const SWITCHERS = document.querySelectorAll('.switch');
+
