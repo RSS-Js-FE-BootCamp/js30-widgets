9f84afdf4ce2b3887f1ad9f01857c1937fc97ee2
Vladislav <vlad.kvan96@gmail.com>
2026-07-02 19:42:43 +0300

    feat: add mandatory additional feature

--- students/vladislav6/photofilter/js/main.js
+++ students/vladislav6/photofilter/js/main.js
@@ -1,10 +1,26 @@
+import './mandatory.js';
+
 const filtersElement = document.querySelector('.filters');
+export const filterInputs = filtersElement.querySelectorAll('input');
 const picture = document.querySelector('.picture-edit');
 const highlight = document.querySelector('.highlight');
+const showAside = document.querySelector('.aside-btn');
+const preview = document.querySelector('.preview');
+const gallery = document.querySelector('.gallery');
 
-const spacing = document.getElementById('spacing');
-const blurInput = document.getElementById('blur');
-const baseColor = document.getElementById('color');
+const showFilters = () => {
+  filtersElement.classList.toggle('show');
+  if (filtersElement.classList.contains('show')) {
+    showAside.textContent = 'Hide filters';
+    preview.style.marginLeft = '0px';
+    gallery.style.width = 'calc(100% - 320px)';
+    gallery.style.marginLeft = '0px';
+  } else {
+    showAside.textContent = 'Show filters';
+    preview.style.marginLeft = 'auto';
+    gallery.style.width = '100%';
+  }
+};
 
 const filterState = function(...filters) {
   return filters.reduce((state, filter) => {
@@ -16,10 +32,33 @@ const filterState = function(...filters) {
 };
 
 const setFiltersToPicture = (filters) => {
-  highlight.style.color = filters.color;
-  picture.style.padding = `${filters.spacing}px`;
-  picture.style.background = filters.color;
-  picture.style.filter = `blur(${filters.blur}px)`;
+  const {
+    spacing,
+    blur,
+    color,
+    brightness,
+    contrast,
+    grayscale,
+    hue,
+    invert,
+    saturate,
+    sepia
+  } = filters;
+
+  const filterTools = `
+    blur(${blur}px)
+    brightness(${brightness})
+    contrast(${contrast})
+    grayscale(${grayscale}%)
+    hue-rotate(${hue}deg)
+    invert(${invert}%)
+    saturate(${saturate})
+    sepia(${sepia}%)
+    `; 
+  highlight.style.color = color;
+  picture.style.padding = `${spacing}px`;
+  picture.style.background = color;
+  picture.style.filter = filterTools;
 };
 
 const onPictureEdit = (e) => {
@@ -29,7 +68,19 @@ const onPictureEdit = (e) => {
   setFiltersToPicture(filters);
 };
 
-const filters = filterState(spacing, blurInput, baseColor);
+const filters = filterState(...filterInputs);
 setFiltersToPicture(filters);
 
+filtersElement.addEventListener('change', onPictureEdit);
 filtersElement.addEventListener('input', onPictureEdit);
+showAside.addEventListener('click', showFilters);
+window.addEventListener('resize', () => {
+  if (
+    window.innerWidth >= 900 &&
+    preview.hasAttribute('style')
+  ) {
+    filtersElement.classList.remove('show');
+    preview.removeAttribute('style');
+    gallery.removeAttribute('style');
+  }
+});
