8a468b1bc011385038c2719f72051aa60bc86eaa
Victoria Vrublevskaya <vivrubl16@gmail.com>
2026-07-02 18:34:37 +0400

    feat: add presets gallery

--- students/VrubDev/photofilter/script.js
+++ students/VrubDev/photofilter/script.js
@@ -1,4 +1,7 @@
 const inputs = document.querySelectorAll(".controls input");
+const presets = document.querySelectorAll(".preset-item");
+
+console.log(presets);
 
 function handleUpdate() {
   const suffix = this.dataset.sizing || "";
@@ -8,4 +11,24 @@ function handleUpdate() {
   );
 }
 
+function applyPreset() {
+  const data = this.dataset;
+
+  for (const filter in data) {
+    const input = document.querySelector(`input[name="${filter}"]`);
+
+    if (input) {
+      input.value = data[filter];
+
+      const suffix = input.dataset.sizing || "";
+
+      document.documentElement.style.setProperty(
+        `--${filter}`,
+        data[filter] + suffix,
+      );
+    }
+  }
+}
+
 inputs.forEach((input) => input.addEventListener("input", handleUpdate));
+presets.forEach((preset) => preset.addEventListener("click", applyPreset));
