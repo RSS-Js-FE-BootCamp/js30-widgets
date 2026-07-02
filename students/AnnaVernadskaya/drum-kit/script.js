a3b1df42eafa96ffe007bc568ef193377592dec3
Anna Vernadskaya <anna-vernadskaya@yandex.ru>
2026-07-02 11:57:01 +0500

    feat: add theme switching

--- students/AnnaVernadskaya/drum-kit/script.js
+++ students/AnnaVernadskaya/drum-kit/script.js
@@ -1,5 +1,7 @@
+//ОСНОВНОЙ ФУНКЦИОНАЛ
 const keys = document.querySelectorAll('.key');
 
+//удаляет класс анимации, когда событие закончилось
 const removeTransition = (event) => {
   if (event.propertyName !== 'transform') {
     return;
@@ -8,6 +10,7 @@ const removeTransition = (event) => {
   event.target.classList.remove('playing');
 }
 
+//воспроизведение аудио при нажатии на клавишу
 const playSound = (event) => {
   const audio = document.querySelector(`audio[data-key="${event.code}"]`);
   const key = document.querySelector(`.key[data-key="${event.code}"]`);
@@ -22,8 +25,29 @@ const playSound = (event) => {
   audio.play();
 }
 
+//слушатель окончания анимации
 keys.forEach((key) => {
   key.addEventListener('transitionend', removeTransition);
 });
 
 window.addEventListener('keydown', playSound);
+
+
+// ПЕРЕКЛЮЧЕНИЕ ТЕМ
+const themeButtons = document.querySelectorAll('.theme-switcher__button');
+const body = document.body;
+
+themeButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const selectedTheme = button.dataset.theme;
+
+    body.classList.remove('theme-demo', 'theme-custom');
+    body.classList.add(`theme-${selectedTheme}`);
+
+    themeButtons.forEach((button) => {
+      button.classList.remove('theme-switcher__button--active');
+    });
+
+    button.classList.add('theme-switcher__button--active');
+  });
+});
