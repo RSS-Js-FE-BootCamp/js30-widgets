b6283fd0b8bf5d2caaee6e295013b4319f2bfc6b
Maksim Safonov <stmslashikk@mail.ru>
2026-07-03 23:21:23 +0300

    feat: add slide switching logic

--- students/SLashikkk/vertical-slider/script.js
+++ students/SLashikkk/vertical-slider/script.js
@@ -3,8 +3,8 @@ const sliderContainer = document.querySelector(".slider__container");
 const slideRight = document.querySelector(".right__side");
 const slideLeft = document.querySelector(".left__side");
 
-const upButton = document.querySelector(".btn__prev");
-const downButton = document.querySelector(".btn__next");
+const upButton = document.querySelector(".btn__next");
+const downButton = document.querySelector(".btn__prev");
 
 const slidesLength = document.querySelectorAll(".right__side li").length;
 
@@ -13,3 +13,25 @@ console.log(slidesLength);
 let activesIndex = 0;
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+
+  if (direction === "up") {
+    activesIndex++;
+  }
+  if (activesIndex > slidesLength - 1) {
+    activesIndex = 0;
+  } else if (direction === "down") {
+    activesIndex--;
+    if (activesIndex < 0) {
+      activesIndex = slidesLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${activesIndex * sliderHeight}px)`;
+  slideLeft.style.transform = `translateY(${activesIndex * sliderHeight}px)`;
+};
