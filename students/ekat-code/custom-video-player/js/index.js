d8a8960377f9bb1d02b06aada3e9a4278935242f
Ekaterina Yurina <igra.ca@yandex.ru>
2026-07-04 09:23:58 +1000

    feat: add toggle sound on/off

--- students/ekat-code/custom-video-player/js/index.js
+++ students/ekat-code/custom-video-player/js/index.js
@@ -1,5 +1,6 @@
 const video = document.querySelector('video');
 const btnPlayPause = document.querySelector('.button__play-pause');
+const btnVolume = document.querySelector('.button__volume');
 const btnFullscreen = document.querySelector('.button__fullscreen');
 
 // keystrokes
@@ -7,9 +8,10 @@ const handleKeyboard = (e) => {
   if (e.code === 'Space') {
     e.preventDefault();
     playPauseVideo();
-  }
-
-  if (e.code === 'KeyF') {
+  } else if (e.code === 'KeyM') {
+    e.preventDefault();
+    controlTheSound();
+  } else if (e.code === 'KeyF') {
     e.preventDefault();
     enterFullscreen();
   }
@@ -24,6 +26,11 @@ const playPauseVideo = () => {
   video.paused ? video.play() : video.pause();
 };
 
+// implement volume
+const controlTheSound = () => {
+  video.muted = !video.muted;
+};
+
 // implement fullscreen
 const enterFullscreen = () => {
   document.fullscreenElement ? document.exitFullscreen() : video.requestFullscreen();
@@ -31,4 +38,5 @@ const enterFullscreen = () => {
 
 addKeyboardEvent();
 btnPlayPause.addEventListener('click', playPauseVideo);
+btnVolume.addEventListener('click', controlTheSound);
 btnFullscreen.addEventListener('click', enterFullscreen);
