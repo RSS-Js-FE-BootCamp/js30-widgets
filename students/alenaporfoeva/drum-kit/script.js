f10f762b80f6ebb05163d686e21155f81a2e560b
Alena <alenaporfoevakorr@gmail.com>
2026-07-04 22:11:02 +0200

    feat: dark mode

--- students/alenaporfoeva/drum-kit/script.js
+++ students/alenaporfoeva/drum-kit/script.js
@@ -36,3 +36,14 @@ document.addEventListener('keydown', (event) => {
         animate(event.code);
     }
 });
+
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains('dark-theme')) {
+        themeToggle.textContent = '☀️ Light Mode';
+    } else {
+        themeToggle.textContent = '🌙 Dark Mode';
+    }
+});
