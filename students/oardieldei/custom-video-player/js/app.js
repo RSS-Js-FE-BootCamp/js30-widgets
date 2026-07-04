327de24964586b72ed998a71ee299e082af3f155
Oardieldei <alik.want.you@gmail.com>
2026-07-04 17:09:58 +0300

    feat: add theme changer

--- students/oardieldei/custom-video-player/js/app.js
+++ students/oardieldei/custom-video-player/js/app.js
@@ -1,5 +1,8 @@
 import { fillCardsContainer } from "./cards-media.js"
+import { addThemeChanger, loadTheme } from "./theme-changer.js"
 
 export function runApp() {
 	fillCardsContainer()
+	loadTheme()
+	addThemeChanger()
 }
