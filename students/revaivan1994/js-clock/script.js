a7bbc73fbeadddd6fb16a83d2c85e307dd4e5af7
Твоё Имя <@email.com>
2026-07-05 18:59:01 +0300

    add js func update clock in js clock

--- students/revaivan1994/js-clock/script.js
+++ students/revaivan1994/js-clock/script.js
@@ -10,3 +10,43 @@ const digitalYear = document.querySelector('.digital-year');
 const cities = document.querySelectorAll('.city');
 const themeButton = document.querySelector('.theme-button');
 
+function addZero(number) {
+  if (number < 10) {
+    return `0${number}`;
+  }
+
+  return number;
+}
+
+function updateClock() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondDegrees = seconds * 6;
+  const minuteDegrees = minutes * 6 + seconds * 0.1;
+  const hourDegrees = (hours % 12) * 30 + minutes * 0.5;
+
+  secondHand.style.transform = `translateX(-50%) rotate(${secondDegrees}deg)`;
+  minuteHand.style.transform = `translateX(-50%) rotate(${minuteDegrees}deg)`;
+  hourHand.style.transform = `translateX(-50%) rotate(${hourDegrees}deg)`;
+
+  digitalTime.textContent = `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`;
+
+  digitalWeekday.textContent = new Intl.DateTimeFormat(locale, {
+    weekday: 'long',
+  }).format(now);
+
+  digitalDate.textContent = new Intl.DateTimeFormat(locale, {
+    month: 'long',
+    day: 'numeric',
+  }).format(now);
+
+  digitalYear.textContent = new Intl.DateTimeFormat(locale, {
+    year: 'numeric',
+  }).format(now);
+
+  updateCitiesTime(now);
+}
