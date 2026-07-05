d8c5bf76cd44702f4dcd13d5240f2fab26460c95
HunterTigerX <HunterTigerX@gmail.com>
2026-07-05 14:36:05 +0300

    feat: complete tasks 2 and 3  05.07.2026 14:36:04

--- students/HunterTigerX/custom-video-player/script.js
+++ students/HunterTigerX/custom-video-player/script.js
@@ -75,6 +75,15 @@ function changeVideoTimeMouse(event) {
     video.currentTime = newTime;
 }
 
+function changeVideoTimeKeyboard(time) {
+    const duration = video.duration;
+    let newTime = duration / 100 * time
+    if (newTime <= 0) {
+        newTime = 0
+    }
+    video.currentTime = newTime;
+}
+
 
 let mousedown = false;
 timelineWrapper.addEventListener('click', (event) => {
@@ -94,16 +103,225 @@ document.addEventListener('mouseup', () => {
 });
 
 
-volumeRange.addEventListener('input', (event) => handleVolumeChange(event.target.value));
-volumeRange.addEventListener('change', (event) => handleVolumeChange(event.target.value));
+volumeRange.addEventListener('input', (event) => {
+    handleVolumeChange(
+        event.target.value
+    )
+});
 
-speedRange.addEventListener('input', (event) => handleSpeedChange(event.target.value));
+volumeRange.addEventListener('change', (event) => {
+    handleVolumeChange(event.target.value)
+});
+
+speedRange.addEventListener('input', (event) => {
+    handleSpeedChange(event.target.value)
+});
+
 speedRange.addEventListener('change', (event) => handleSpeedChange(event.target.value));
 
 function handleVolumeChange(volumeChange) {
-    video.volume = volumeChange / 100
+
+    video.volume = volumeChange;
+
+    if (video.volume == 0) {
+        video.classList.add('mute')
+    } else {
+        video.classList.remove('mute')
+    }
 }
 
 function handleSpeedChange(speedChange) {
-    video.playbackRate = 0.5 + (speedChange / 100) * 3.5;
+    video.playbackRate = speedChange;
+}
+
+let lastVolume = video.volume;
+
+document.addEventListener('keydown', function (e) {
+
+    if (e.key == " " ||
+        e.code == "Space" ||
+        e.keyCode == 32 ||
+
+        e.key == "k" ||
+        e.code == "KeyK" ||
+        e.keyCode == 75 ||
+        e.key == 'л'
+    ) {
+        if (e.code == "Space") {
+            e.preventDefault();
+        }
+        toggleVideo();
+    }
+
+    if (e.key.toLowerCase() === "M" || e.key.toLowerCase() === "Ь" || e.keyCode == 77) {
+        if (video.classList.contains('mute')) {
+            video.classList.remove('mute')
+            video.volume = lastVolume;
+            volumeRange.value = lastVolume;
+            lastVolume = 0;
+        } else {
+            video.classList.add('mute')
+            lastVolume = video.volume;
+            video.volume = 0;
+            volumeRange.value = 0;
+        }
+    }
+
+    if ((e.shiftKey && (e.key === ">" || e.key === "Ю" || e.key === "."))) {
+        if (Number(speedRange.value) < 2) {
+            video.playbackRate += 0.1;
+            speedRange.value = Number(speedRange.value) + 0.1;
+        }
+    }
+
+
+    if (e.shiftKey && (e.key === "<" || e.key === "Б" || e.key === ",")) {
+        if (Number(speedRange.value) > 0.5) {
+            video.playbackRate -= 0.1;
+            speedRange.value = Number(speedRange.value) - 0.1;
+        }
+    }
+
+    if (!e.shiftKey && (e.key === "<" || e.key.toLowerCase() === "б" || e.keyCode == 188)) {
+        if (!playerControl.classList.contains('play')) {
+            video.playbackRate -= 0.1;
+            speedRange.value = Number(speedRange.value) - 0.1;
+        } else {
+            changeFrame('backward')
+        }
+    }
+
+
+    if (!e.shiftKey && (e.key === ">" || e.key.toLowerCase() === "ю" || e.keyCode == 190)) {
+        if (!playerControl.classList.contains('play')) {
+            video.playbackRate += 0.1;
+            speedRange.value = Number(speedRange.value) + 0.1;
+        } else {
+            changeFrame('forward')
+        }
+    }
+
+
+
+
+
+
+
+    if (e.key.toLowerCase() === "F" || e.key.toLowerCase() === "А" || e.keyCode == 70) {
+        if (video.classList.contains('fullscreen')) {
+            closeFullscreen()
+            video.classList.remove('fullscreen')
+        } else {
+            openFullscreen()
+            video.classList.add('fullscreen')
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if (e.key.toLowerCase() === "0" || e.key.toLowerCase() === "0" || e.keyCode == 48) {
+        video.currentTime = 0
+    }
+    if (e.key.toLowerCase() === "1" || e.key.toLowerCase() === "1" || e.keyCode == 49) {
+        changeVideoTimeKeyboard(10)
+    }
+    if (e.key.toLowerCase() === "2" || e.key.toLowerCase() === "2" || e.keyCode == 50) {
+        changeVideoTimeKeyboard(20)
+    }
+    if (e.key.toLowerCase() === "3" || e.key.toLowerCase() === "3" || e.keyCode == 51) {
+        changeVideoTimeKeyboard(30)
+    }
+    if (e.key.toLowerCase() === "4" || e.key.toLowerCase() === "4" || e.keyCode == 52) {
+        changeVideoTimeKeyboard(40)
+    }
+    if (e.key.toLowerCase() === "5" || e.key.toLowerCase() === "5" || e.keyCode == 53) {
+        changeVideoTimeKeyboard(50)
+    }
+    if (e.key.toLowerCase() === "6" || e.key.toLowerCase() === "6" || e.keyCode == 54) {
+        changeVideoTimeKeyboard(60)
+    }
+    if (e.key.toLowerCase() === "7" || e.key.toLowerCase() === "7" || e.keyCode == 55) {
+        changeVideoTimeKeyboard(70)
+    }
+    if (e.key.toLowerCase() === "8" || e.key.toLowerCase() === "8" || e.keyCode == 56) {
+        changeVideoTimeKeyboard(80)
+    }
+    if (e.key.toLowerCase() === "9" || e.key.toLowerCase() === "9" || e.keyCode == 57) {
+        changeVideoTimeKeyboard(90)
+    }
+
+
+
+
+
+
+    if (e.key.toLowerCase() === "j" || e.key.toLowerCase() === "о" || e.keyCode == 74) {
+        video.currentTime = Number(video.currentTime) - 10;
+    }
+    if (e.key.toLowerCase() === "l" || e.key.toLowerCase() === "д" || e.keyCode == 76) {
+        video.currentTime = Number(video.currentTime) + 10;
+    }
+
+
+
+
+
+
+})
+
+
+
+
+
+function openFullscreen() {
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+    } else if (video.webkitRequestFullscreen) {
+        video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) {
+        video.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
+
+
+function changeFrame(type) {
+    const fps = video.getVideoPlaybackQuality?.()?.totalVideoFrames /
+        video.getVideoPlaybackQuality?.()?.totalFrameDelay || 30;
+    const frameStep = 1 / fps;
+    if (type === 'backward') {
+
+        video.currentTime = Math.max(Number(video.currentTime - frameStep, 0));
+    } else if (type === 'forward') {
+        video.currentTime = Math.max(Number(video.currentTime + frameStep, 0));
+
+    }
+
 }
