12f8fad8e8d533a8fe3978aa5d428427e4ace257
Твоё Имя <@email.com>
2026-07-05 18:56:19 +0300

    add js variables consts in js code in js clock

---
+++ students/revaivan1994/js-clock/script.js
@@ -0,0 +1,12 @@
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+const digitalTime = document.querySelector('.digital-time');
+const digitalWeekday = document.querySelector('.digital-weekday');
+const digitalDate = document.querySelector('.digital-date');
+const digitalYear = document.querySelector('.digital-year');
+
+const cities = document.querySelectorAll('.city');
+const themeButton = document.querySelector('.theme-button');
+
