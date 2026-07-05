095bee4b3dbcf49777d7da80194131e783d1892b
Ivan <ivankonovalov96@inbox.ru>
2026-07-05 17:19:43 +0500

    feat: add light theme

--- students/IvanK9/drum-kit/script.js
+++ students/IvanK9/drum-kit/script.js
@@ -1,5 +1,26 @@
+// THEME
+
+const themeBtn = document.querySelector(".header__btn--theme");
+
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light-theme");
+}
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+
+  if (document.body.classList.contains("light-theme")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
+});
+
+// PLAY
+
 function playSound(e) {
-  const keyCode = e.type === "keydown" ? e.keyCode : e.currentTarget.getAttribute('data-key');
+  const keyCode =
+    e.type === "keydown" ? e.keyCode : e.currentTarget.getAttribute("data-key");
   const sound = document.querySelector(`audio[data-key="${keyCode}"]`);
   const list = document.querySelector(".keys__list");
   const el = list.querySelector(`.keys__item[data-key="${keyCode}"]`);
@@ -19,7 +40,7 @@ function removeTransition(e) {
 const keys = document.querySelectorAll(".keys__item");
 
 keys.forEach((key) => {
-  key.addEventListener('click', playSound);
+  key.addEventListener("click", playSound);
   key.addEventListener("transitionend", removeTransition);
 });
 
